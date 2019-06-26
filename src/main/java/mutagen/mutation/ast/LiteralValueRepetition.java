8d7350f9fa6761764b3e2849c74ae7c2ecf56220
Ben Clegg <bclegg@midcode.co.uk>
2019-06-26 16:10:28 +0100

    added LiteralValueRepetition

--- src/main/java/mutagen/mutation/ast/LiteralValueRepetition.java
+++ src/main/java/mutagen/mutation/ast/LiteralValueRepetition.java
@@ -5,20 +5,27 @@ import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
+import com.github.javaparser.ast.nodeTypes.NodeWithVariables;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import mutagen.TargetSource;
 import mutagen.mutation.ast.identifiernaming.NameReformatter;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class LiteralValueRepetition extends ASTVisitorMutationStrategy
 {
+    final Class[] NODE_TYPES_TO_REPLACE = new Class[]{
+            com.github.javaparser.ast.expr.NameExpr.class
+    };
+
     public LiteralValueRepetition(TargetSource target)
     {
         super(target);
         setType("LiteralValueRepetition");
     }
+
 
     @Override
     protected void visitorSetup()
@@ -26,32 +33,19 @@ public class LiteralValueRepetition extends ASTVisitorMutationStrategy
         visitor = new VoidVisitorAdapter<Void>()
         {
             @Override
-            public void visit(FieldDeclaration declaration, Void v)
+            public void visit(FieldDeclaration n, Void v)
             {
-
-                super.visit(declaration, v);
-                generateConstantReplacementMutant(declaration, new Class[]{
-                        com.github.javaparser.ast.body.Parameter.class
-                });
+                super.visit(n, v);
+                generateConstantReplacementMutant(n, NODE_TYPES_TO_REPLACE);
             }
 
 
             @Override
-            public void visit(VariableDeclarationExpr n, Void arg)
+            public void visit(VariableDeclarationExpr n, Void v)
             {
-                super.visit(n, arg);
-                System.err.println(n.toString());
-                //TODO also implement for var decl
+                super.visit(n, v);
+                generateConstantReplacementMutant(n, NODE_TYPES_TO_REPLACE);
             }
-
-            /*
-            @Override
-            public void visit(FieldDeclaration n, Void arg)
-            {
-                super.visit(n, arg);
-                System.err.println(n.toString());
-            }
-            */
 
             /*
             @Override
@@ -65,34 +59,51 @@ public class LiteralValueRepetition extends ASTVisitorMutationStrategy
         };
     }
 
-    private void generateConstantReplacementMutant(FieldDeclaration declaration, Class[] usageNodeTypes)
+    private void generateConstantReplacementMutant(NodeWithVariables declaration, Class[] usageNodeTypes)
     {
-        // TODO extract value by searching children for literals
 
-        System.err.println(varAssignment.toString());
-
-        // Skip if not a literal
-        if (!varAssignment.isLiteralExpr())
-            return;
+        //System.err.println(declaration.toString());
 
         // Extract value
-        LiteralExpr literal = varAssignment.getValue().asLiteralExpr();
+        //LiteralExpr literal = declaration.getValue().asLiteralExpr();
 
         // Iterate over declarators associated with assignment
-        for (VariableDeclarator declarator : varAssignment.getTarget().asVariableDeclarationExpr().getVariables())
+        //for (VariableDeclarator varDec : declaration.getVariables())
+        for (Object variableDeclarator : declaration.getVariables())
         {
+            VariableDeclarator varDec = (VariableDeclarator)variableDeclarator;
+
             // Skip if declarator is not a constant
-            if (!NameReformatter.isConstant(declarator.getNameAsString()))
+            // TODO check for final keyword instaead
+            if (!NameReformatter.isConstant(varDec.getNameAsString())) {
                 break;
+            }
 
-            // Find usages of the identifier, and make patches
-            List<NodePatch> nodePatches = generateIdentifierValuePatches(declarator.getName(), literal, usageNodeTypes);
+            // extract value by searching children for literals
+            Iterator<Node> nodeIterator = varDec.getChildNodes().stream()
+                    .filter(n -> (n instanceof LiteralExpr))
+                    .iterator();
 
-            // Add full mutant
-            ASTMutant m = new ASTMutant(getOriginal().getCompilationUnit(), nodePatches, type);
-            m.setPreMutation(declarator.getNameAsString());
-            m.setPostMutation(literal.toString());
-            addMutant(m);
+            while(nodeIterator.hasNext())
+            {
+                Node n = nodeIterator.next();
+
+                // Already filtered; Contains value
+                LiteralExpr literal = (LiteralExpr) n;
+
+                // Find usages of the identifier, and make patches
+                List<NodePatch> nodePatches = generateIdentifierValuePatches(varDec.getName(), literal, usageNodeTypes);
+
+                // Add full mutant
+                ASTMutant m = new ASTMutant(getOriginal().getCompilationUnit(), nodePatches, type);
+                m.setPreMutation(varDec.getNameAsString());
+                m.setPostMutation(literal.toString());
+                addMutant(m);
+            }
+
+
+
+
         }
 
     }
